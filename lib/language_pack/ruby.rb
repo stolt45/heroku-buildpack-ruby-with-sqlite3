7b30b015d21694fc5bba519f7a22cf5034acd7cc
Christopher Stolt <stolt45@gmail.com>
2015-09-03 15:59:06 -0700

    straight up gem install

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -564,7 +564,7 @@ WARNING
           puts `ls -lah #{File.expand_path(  '../../vendor/sqlite3/bin', $PROGRAM_NAME )}`
 
           #puts `ls #{File.expand_path(  '../../.bundle/', $PROGRAM_NAME )}`
-          run("gem install sqlite3 -- --with-sqlite3-dir=#{File.expand_path(  '../../vendor/sqlite3/bin', $PROGRAM_NAME )} --srcdir=#{File.expand_path(  '../../vendor/sqlite3', $PROGRAM_NAME )} --with-sqlite3-lib=#{File.expand_path(  '../../vendor/sqlite3/lib', $PROGRAM_NAME )} ")
+          puts `gem install sqlite3 -- --with-sqlite3-dir=#{File.expand_path(  '../../vendor/sqlite3/bin', $PROGRAM_NAME )} --srcdir=#{File.expand_path(  '../../vendor/sqlite3', $PROGRAM_NAME )} --with-sqlite3-lib=#{File.expand_path(  '../../vendor/sqlite3/lib', $PROGRAM_NAME )}`
           #puts "Adding new bundle config"
           #run("bundle config --global build.sqlite3 -- --with-sqlite3-dir=#{File.expand_path(  '../../vendor/sqlite3/bin', $PROGRAM_NAME )} --srcdir=#{File.expand_path(  '../../vendor/sqlite3', $PROGRAM_NAME )} --with-sqlite3-lib=#{File.expand_path(  '../../vendor/sqlite3/lib', $PROGRAM_NAME )} ")
           #puts `bundle config`
